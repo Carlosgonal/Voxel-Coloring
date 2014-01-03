c3d41d1dfa5874c63f3422905a9e0cdcaf507be8
Carlos González <Carlos@MacBook-Pro-de-Carlos.local>
2014-01-03 17:27:28 +0100

    Initial Commit

---
+++ ProyectoVC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProyectoVC
+//
+//  Created by Carlos González on 03/01/14.
+//  Copyright (c) 2014 Carlos González. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
